f9c247a20003d8d9defa8a068a1d5a27afbe6254
SiddhiikaN <siddhikanagarkar@gmail.com>
2024-06-13 19:20:52 +0530

    Added Java files for FlappyBird game

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("FlappyBird");
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FlappyBird flappybird = new FlappyBird();
+        frame.add(flappybird);
+        frame.pack();
+        flappybird.requestFocus();
+        frame.setVisible(true);
+
+    }
+}
